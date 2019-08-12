e8feff6b84f8e5a16a85e7d651fd597ca0cebc48
Tim Willis <tcwillis1@gmail.com>
2019-08-12 19:26:15 +1000

    Added thunk to middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,31 @@
 import { createBrowserHistory } from "history";
 import { applyMiddleware, compose, createStore } from "redux";
 import { routerMiddleware } from "connected-react-router";
+import thunk from "redux-thunk";
 import createRootReducer from "./reducers";
 export const history = createBrowserHistory();
+
+const enhancers = [];
+const middleware = [thunk, routerMiddleware(history)];
+
+if (process.env.NODE_ENV === "development") {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === "function") {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+);
 
 export default function configureStore(preloadedState) {
   const store = createStore(
     createRootReducer(history),
     preloadedState,
-    compose(
-      applyMiddleware(
-        routerMiddleware(history)
-        // ... other middlewares ...
-      )
-    )
+    composedEnhancers
   );
 
   return store;
